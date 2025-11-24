e8c314ce131d5a57f71f56a6543c732bf0c9b302
Kathe Arguello <programacionkathe04@gmail.com>
2025-11-24 15:12:52 -0500

    Add files via upload

---
+++ finaljuanfe14-main/finaljuanfe14-main/controlador.js
@@ -0,0 +1,20 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+
+    let cajaUsaurio=document.getElementById("cajausuario")
+    let cajaCorreo=document.getElementById("cajacorreo")
+    let cajaCiudad=document.getElementById("cajaciudad")
+    let cajaTelefono=document.getElementById("cajatelefono")
+    let cajaContraseña=document.getElementById("cajacontraseña")
+
+    let datosDelFormulario={
+        usuario:cajaUsaurio.value,
+        correo:cajaCorreo.value,
+        ciudad:cajaCiudad.value,
+        telefono:cajaTelefono.value,
+        contraseña:cajaContraseña.value,
+
+    }
+
+})
